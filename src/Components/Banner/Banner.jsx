c7c9739c2ea9678f6145027d21e1fb2c643b8dee
Abu Ratin Shoeb <aburatinshoeb@MacBook-Air.local>
2023-12-22 02:03:51 +0600

    clickable social icons in footer

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold py-3 md:py-10 leading-tight">Ease Your Developer Life</h1>
+          <h1 className="mb-5 text-5xl font-bold py-3 md:py-10 uppercase leading-tight">Ease Your Developer Life</h1>
 
           <button className="btn bg-purple-500 text-white hover:bg-purple-700 border-0">
             Explore Now!
